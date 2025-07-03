bbb9a7d165617cd28179077d3cf60be7dd26529f
happyfacehappyface <cr0204a@gmail.com>
2025-07-03 20:00:59 +0900

    Add: Tab Converting Buttons

--- app/src/main/java/com/example/nookstyle/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/nookstyle/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.nookstyle.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nookstyle.R
@@ -12,12 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ItemAdapter
+    private lateinit var tab1: Button
+    private lateinit var tab2: Button
+    private lateinit var tab3: Button
+    private lateinit var tab2Content: View
+    private lateinit var tab3Content: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
+        // 뷰 초기화
         recyclerView = findViewById(R.id.recyclerView)
+        tab1 = findViewById(R.id.tab1)
+        tab2 = findViewById(R.id.tab2)
+        tab3 = findViewById(R.id.tab3)
+        tab2Content = findViewById(R.id.tab2Content)
+        tab3Content = findViewById(R.id.tab3Content)
         
         // GridLayoutManager 사용 - 2열로 설정
         recyclerView.layoutManager = GridLayoutManager(this, 2)
@@ -42,9 +55,76 @@ class MainActivity : AppCompatActivity() {
             Item("제목 16", "설명 16"),
             Item("제목 17", "설명 17"),
         )
-        
 
         adapter = ItemAdapter(itemList)
         recyclerView.adapter = adapter
+
+        // 탭 클릭 이벤트 설정
+        setupTabClickListeners()
+    }
+
+    private fun setupTabClickListeners() {
+        tab1.setOnClickListener {
+            showTab1()
+        }
+        
+        tab2.setOnClickListener {
+            showTab2()
+        }
+        
+        tab3.setOnClickListener {
+            showTab3()
+        }
+    }
+
+    private fun showTab1() {
+        // 탭 1 활성화
+        tab1.setBackgroundColor(getColor(android.R.color.holo_blue_light))
+        tab1.setTextColor(getColor(android.R.color.white))
+        
+        tab2.setBackgroundColor(getColor(android.R.color.white))
+        tab2.setTextColor(getColor(android.R.color.black))
+        
+        tab3.setBackgroundColor(getColor(android.R.color.white))
+        tab3.setTextColor(getColor(android.R.color.black))
+
+        // 콘텐츠 전환
+        recyclerView.visibility = View.VISIBLE
+        tab2Content.visibility = View.GONE
+        tab3Content.visibility = View.GONE
+    }
+
+    private fun showTab2() {
+        // 탭 2 활성화
+        tab1.setBackgroundColor(getColor(android.R.color.white))
+        tab1.setTextColor(getColor(android.R.color.black))
+        
+        tab2.setBackgroundColor(getColor(android.R.color.holo_blue_light))
+        tab2.setTextColor(getColor(android.R.color.white))
+        
+        tab3.setBackgroundColor(getColor(android.R.color.white))
+        tab3.setTextColor(getColor(android.R.color.black))
+
+        // 콘텐츠 전환
+        recyclerView.visibility = View.GONE
+        tab2Content.visibility = View.VISIBLE
+        tab3Content.visibility = View.GONE
+    }
+
+    private fun showTab3() {
+        // 탭 3 활성화
+        tab1.setBackgroundColor(getColor(android.R.color.white))
+        tab1.setTextColor(getColor(android.R.color.black))
+        
+        tab2.setBackgroundColor(getColor(android.R.color.white))
+        tab2.setTextColor(getColor(android.R.color.black))
+        
+        tab3.setBackgroundColor(getColor(android.R.color.holo_blue_light))
+        tab3.setTextColor(getColor(android.R.color.white))
+
+        // 콘텐츠 전환
+        recyclerView.visibility = View.GONE
+        tab2Content.visibility = View.GONE
+        tab3Content.visibility = View.VISIBLE
     }
 }
